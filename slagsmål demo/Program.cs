04c27914ef21d002c70138827c4662558e595421
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2024-09-20 10:27:42 +0200

    jhhh5

--- slagsmål demo/Program.cs
+++ slagsmål demo/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿int card = Random.Shared.Next(52);
+System.Console.WriteLine(card);
+
+System.Console.WriteLine("Skriv in ditt namn");
+String name = Console.ReadLine();
+
+while (name.Length < 3)
+{
+    System.Console.WriteLine("Du får inte");
+    name = Console.ReadLine();
+}
+
+Console.WriteLine($"hej {name}");
+
+
+
+Console.ReadLine();
